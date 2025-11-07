2321d5847f4b688167f96d50f922f37d890b4c3b
Walter Gomes <josewaltergomes13@gmail.com>
2025-11-07 19:47:13 -0300

    App com datasets de defeitos e modelo de aprendizado com  prediçao de defeitos concluido

---
+++ predictModel.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+df_sample = pd.read_csv('./sample_data/dataset_de_features.csv')
+df_sample.info()
+
+X = df_sample.drop('fault', axis = 1)
+y = df_sample['fault']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, 
+    test_size=0.25, 
+    random_state=42, 
+    stratify=y
+)
+
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+print(f"Training set size: {X_train.shape}, Testing set size: {X_test.shape}")
+
+rf = RandomForestClassifier(
+    n_estimators=500,
+    max_depth=15,
+    random_state=42,
+    n_jobs=-1
+)
+rf.fit(X_train, y_train)
+importances = rf.feature_importances_
+
+idx = np.argsort(importances)[-200:]
+
+X_train_sel = X_train[:, idx]
+X_test_sel = X_test[:, idx]
+
+rf2 = RandomForestClassifier(
+    n_estimators=500,
+    max_depth=15,
+    random_state=42,
+    n_jobs=-1
+)
+
+rf2.fit(X_train_sel, y_train)
+y_pred = rf2.predict(X_test_sel)
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy of the model: {accuracy *100:.2f}%")
+
+'''def previsao(dados):
+    X_data = scaler.transform(dados)
+    X_data_sel = X_data[:, idx]
+    y_pred_data = rf2.predict()
+'''
